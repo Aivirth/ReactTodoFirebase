525b4c98c31c8b09b1327daf04e040e7b7b8547d
Aivirth <aivirth.dev@gmail.com>
2019-04-20 16:09:34 +0200

    Added firebase and firestore config file

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC_X4FeystKjgU6HTS_H2V7LMd3GaFkPsg",
+  authDomain: "reacttodolist-f083c.firebaseapp.com",
+  databaseURL: "https://reacttodolist-f083c.firebaseio.com",
+  projectId: "reacttodolist-f083c",
+  storageBucket: "reacttodolist-f083c.appspot.com",
+  messagingSenderId: "1025019513371"
+};
+
+// Initialize firebase instance
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({
+  timestampsInSnapshots: true
+});
+
+export default firebase;
